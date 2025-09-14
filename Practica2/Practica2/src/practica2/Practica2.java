1d17c648994fd3bd36400c7aa50449123b9a822e
HCasalderrey <el_topo_48@hotmail.com>
2025-09-14 12:26:25 -0300

    Creación del repositorio para Programacion 2

---
+++ Practica2/Practica2/src/practica2/Practica2.java
@@ -0,0 +1,17 @@
+ 
+package practica2;
+
+/**
+ *
+ * HERNAN CASALDERREY - UTN TUPAD 2025 - PRACTICA 2
+ */
+public class Practica2 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
---
+++ README.md
@@ -0,0 +1,19 @@
+## TPs 
+Hernan Casalderrey
+M-100265
+UTN TUPAD
+2025
+
+*Se va a ir actualizando a medida que se vayan subiendo los trabajos practicos*
+
+### TP2
+ Carpeta Practica2
+
+### TP3
+ Carpeta Practica3
+
+### TP4
+ Carpeta Practica4
+    
+### TP5
+ Carpeta Practica5
